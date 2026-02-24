e8be7168285b3dedfe420af68b66fb47adc37399
naya2matter <https://github.com/naya2matter>
2026-02-24 12:58:21 -0500

    feat: add SettingsPage component to render ProfilePage

---
+++ app/dashboard/setting/page.tsx
@@ -0,0 +1,9 @@
+import ProfilePage from "./profile/page";
+
+export default function SettingsPage() {
+    return (
+        <div style={{ padding: 32 }}>
+            <ProfilePage />
+        </div>
+    );
+}
