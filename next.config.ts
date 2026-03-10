2ba2fb6fcf8c28c6e5d1cefba2c0f44faca76527
naya2matter <https://github.com/naya2matter>
2026-03-10 07:36:16 -0400

    https

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
       {
         // Proxy ALL CMS API calls through Next.js to avoid CORS
         source: "/api/:path*",
-        destination: "http://cms.1ftt.com/api/:path*",
+        destination: "https://cms.1ftt.com/api/:path*",
       },
     ];
   },
