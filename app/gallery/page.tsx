606059955966cbecb17035c58b4647240d9d4a15
naya2matter <https://github.com/naya2matter>
2026-03-13 11:12:43 -0400

    Refactor GalleryPage to generate metadata dynamically using fetchHomeData

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,12 +4,21 @@ import GalleryHeroSection from "@/app/components/sections/gallery/GalleryHeroSec
 import GalleryGridSection from "@/app/components/sections/gallery/GalleryGridSection";
 import Footer from "../components/layout/Footer";
 import { fetchGalleryData } from "@/lib/services/gallery.service";
+import { fetchHomeData } from "@/lib/services/home.service";
 
-export const metadata: Metadata = {
-  title: "Life at FTT | First Team Trucking",
-  description:
-    "Explore life at First Team Trucking with highlights of our fleet, facilities, and team culture.",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await fetchHomeData();
+
+  return {
+    title: data?.site_metadata?.name ?? "Life at FTT | First Team Trucking",
+    description:
+      data?.site_metadata?.description ??
+      "Explore life at First Team Trucking with highlights of our fleet, facilities, and team culture.",
+    icons: {
+      icon: data?.site_metadata?.favicon ?? "/favicon.ico",
+    },
+  };
+}
 
 export default async function GalleryPage() {
   const galleryData = await fetchGalleryData();
